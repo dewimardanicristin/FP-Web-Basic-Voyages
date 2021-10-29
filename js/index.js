a1f1c6d85edd945550654100f9a76ff3b02d2b1c
dewimardanic <dewimardanic@gmail.com>
2021-10-29 17:30:31 +0800

    terfix

--- js/index.js
+++ js/index.js
@@ -1,9 +1,11 @@
+const url = window.location.pathname;
+ 
 var haslogin = localStorage.getItem("isLogin");
 const logout = () => {
   localStorage.setItem("isLogin", false);
   window.location.replace('../login.html');
 }
-
+ 
 const cekLogin = () => {
   if (haslogin == "true") {
  
@@ -13,15 +15,15 @@ const cekLogin = () => {
                 <img src="/assets/logo.png" width="40" height="32" alt="">
                 <div class="fw-bolder ps-2">Voyagers</div>
             </a>
-
+ 
             <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
                 <li class="m-2"><a href="./daftar_wisata.html"
-                        class="nav-link px-2 link-dark border-bottom border-dark border-2 navhover">Daftar
+                        class="nav-link px-2 link-dark navhover ${url === "/daftar_wisata.html" && "border-dark border-2 border-bottom"}">Daftar
                         wisata</a>
                 </li>
-                <li class="m-2"><a href="./koleksi-foto.html" class="nav-link px-2 link-dark navhover">Koleksi foto</a></li>
+                <li class="m-2"><a href="./koleksi-foto.html" class="nav-link px-2 link-dark navhover ${url === "/koleksi-foto.html" && "border-dark border-2 border-bottom"}">Koleksi foto</a></li>
             </ul>
-
+ 
             <div class="col-md-3 text-end">
             <div class="d-grid gap-3 d-md-flex justify-content-md-end">
             <div class="dropdown">
@@ -32,13 +34,13 @@ const cekLogin = () => {
                 <li class="mb-3"><a href="./history.html" class="text-decoration-none"><span class="dropdown-item">Riwayat</span></a></li>
                 <li class="m-1"><button type="button" onclick="logout()" class="w-100 btn btn-primary">Keluar</button></li>
               </ul>
-
-              
+ 
+ 
             </div>
         </header>
           `);
   } else {
-
+ 
     return (document.getElementById(
       "container"
     ).innerHTML += `<header
@@ -47,18 +49,18 @@ const cekLogin = () => {
         <img src="/assets/logo.png" width="40" height="32" alt="">
         <div class="fw-bolder ps-2">Voyagers</div>
     </a>
-
+ 
     <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
         <li class="m-2"><a href="./daftar_wisata.html"
-                class="nav-link px-2 link-dark border-bottom border-dark border-2 navhover">Daftar
+                class="nav-link px-2 link-dark navhover ${url === "/daftar_wisata.html" && "border-dark border-2 border-bottom"}">Daftar
                 wisata</a>
         </li>
-        <li class="m-2"><a href="./koleksi-foto.html" class="nav-link px-2 link-dark navhover">Koleksi foto</a></li>
+        <li class="m-2"><a href="./koleksi-foto.html" class="nav-link px-2 link-dark navhover ${url === "/koleksi-foto.html" && "border-dark border-2 border-bottom"}">Koleksi foto</a></li>
     </ul>
-
+ 
     <div class="col-md-3 text-end">
     <a href="./daftar.html">
-        <button type="button" class="btn btn-outline-dark me-2">Sign-up</button>
+        <button type="button" class="btn btn-outline-dark me-2">Sign Up</button>
     </a>
     <a href="./login.html">
         <button type="button" class="btn btn-dark">Login</button>
--- js/login.js
+++ js/login.js
@@ -1,41 +1,49 @@
 const getData = JSON.parse(localStorage.getItem('data'));
-console.log(getData.email)
-console.log(getData.password)
-
+ 
 function showPassword() {
-    
+ 
     var key_attr = $('#key').attr('type');
-    
+ 
     if(key_attr != 'text') {
-        
+ 
         $('.checkbox').addClass('show');
         $('#key').attr('type', 'text');
-        
+ 
     } else {
-        
+ 
         $('.checkbox').removeClass('show');
         $('#key').attr('type', 'password');
-        
+ 
     }
-    
+ 
 }
-
+ 
 const password = document.getElementById("key")
 const email = document.getElementById("email")
 const button = document.querySelector('.btn');
-// console.log(password.value)
-
+ 
 function validate(e){
     e.preventDefault();
-    
-    if(password.value === getData.password && email.value===getData.email){
+ 
+    if(getData!==null && password.value === getData.password && email.value===getData.email){
+        document.getElementById("alert").innerHTML += `
+            <div class="alert alert-success" role="alert">
+                Berhasil Anda akan dialihkan ke halaman utama
+            </div>
+        `
         localStorage.setItem("isLogin",true)
-        window.location = "/"
+        setTimeout(() => {
+            window.location = "/"
+        }, 2000)
     }
-    else if(password.value !== getData.password && email.value!==getData.email){
-        alert("Periksa kembali username dan password anda")
-        console.log("tes");
+    else {
+        document.getElementById("alert").innerHTML += `
+        <div class="alert alert-danger" role="alert" id="alert-fail">
+        Periksa kembali username dan password anda
+        </div>
+        `
+        setTimeout(() => {
+            document.getElementById("alert-fail").remove()
+        }, 2000)
     }
 }
-
-
